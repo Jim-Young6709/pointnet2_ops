3c76637c9c285729d5249ba7983db51acfe032b5
Jiahui(Jim) Yang(Young) <jiahuiya@andrew.cmu.edu>
2024-09-10 02:13:37 -0400

    using precompiled files

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ from setuptools import find_packages, setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 this_dir = osp.dirname(osp.abspath(__file__))
-_ext_src_root = osp.join("pointnet2_ops", "_ext-src")
-_ext_sources = glob.glob(osp.join(_ext_src_root, "src", "*.cpp")) + glob.glob(
-    osp.join(_ext_src_root, "src", "*.cu")
-)
-_ext_headers = glob.glob(osp.join(_ext_src_root, "include", "*"))
+# _ext_src_root = osp.join("pointnet2_ops", "_ext-src")
+# _ext_sources = glob.glob(osp.join(_ext_src_root, "src", "*.cpp")) + glob.glob(
+#     osp.join(_ext_src_root, "src", "*.cu")
+# )
+# _ext_headers = glob.glob(osp.join(_ext_src_root, "include", "*"))
 
 exec(open(osp.join("pointnet2_ops", "_version.py")).read())
 
@@ -20,17 +20,17 @@ setup(
     version=__version__,
     author="Erik Wijmans (Modified by Adam Fishman)",
     packages=find_packages(),
-    ext_modules=[
-        CUDAExtension(
-            name="pointnet2_ops._ext",
-            sources=_ext_sources,
-            extra_compile_args={
-                "cxx": ["-O3"],
-                "nvcc": ["-O3", "-Xfatbin", "-compress-all"],
-            },
-            include_dirs=[osp.join(this_dir, _ext_src_root, "include")],
-        )
-    ],
-    cmdclass={"build_ext": BuildExtension},
+    # ext_modules=[
+    #     CUDAExtension(
+    #         name="pointnet2_ops._ext",
+    #         sources=_ext_sources,
+    #         extra_compile_args={
+    #             "cxx": ["-O3"],
+    #             "nvcc": ["-O3", "-Xfatbin", "-compress-all"],
+    #         },
+    #         include_dirs=[osp.join(this_dir, _ext_src_root, "include")],
+    #     )
+    # ],
+    # cmdclass={"build_ext": BuildExtension},
     include_package_data=True,
 )
